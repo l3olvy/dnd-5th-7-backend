4278a768716d5bae951a12c4f7045d78063586d9
Bon Kim <qhsl0615@naver.com>
2021-08-14 22:48:58 +0900

    httponly

--- app.js
+++ app.js
@@ -53,12 +53,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-	resave: false,
+	resave: true,
 	saveUninitialized: false,
 	secret: process.env.COOKIE_SECRET,
 	cookie: {
+		httpOnly: true,
 		sameSite: 'none',
-		httpsOnly: true,
 		secure: true,
 	},
 }));
