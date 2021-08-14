283c7fc993ddb4026d408de87e9cad57b4b5e442
Bon Kim <qhsl0615@naver.com>
2021-08-14 22:54:23 +0900

    resave false test

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-	resave: true,
+	resave: false,
 	saveUninitialized: false,
 	secret: process.env.COOKIE_SECRET,
 	cookie: {
